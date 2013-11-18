6c380f0e37735a19f2b7ed5367628d1a152a0ae4
Graham Siener <siener@gmail.com>
2013-11-18 17:57:55 -0500

    Test against the secure api endpoints

--- test/helper.rb
+++ test/helper.rb
@@ -6,7 +6,7 @@ require 'pry'
 
 FlickRaw.api_key = ENV['FLICKRAW_API_KEY']
 FlickRaw.shared_secret = ENV['FLICKRAW_SHARED_SECRET']
-#FlickRaw.secure = true
+FlickRaw.secure = true
 
 flickr.access_token = ENV['FLICKRAW_ACCESS_TOKEN']
 flickr.access_secret = ENV['FLICKRAW_ACCESS_SECRET']
